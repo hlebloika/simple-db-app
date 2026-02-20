8d085e45485171ce93ea448669bffa4dfddc59b6
Gleb <gleb.loiko@gmail.com>
2026-02-20 14:52:14 +0100

    added table creation

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 import os
 
 from flask import Flask
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData, Table, Integer, String
 
 app = Flask(__name__)
 
 engine = create_engine(os.getenv('DATABASE_URL'))
+metadata_obj = MetaData()
+
+message_table = Table(
+    "test",
+    metadata_obj,
+    Column("ID", Integer, primary_key=True)
+    Column("message", String)
+)
+
+metadata_obj.create_all(engine)
 
 @app.route("/")
 def hello_world():
