40041e91118fc068dadf6473a2b8d2e893372a6f
Gleb <gleb.loiko@gmail.com>
2026-02-20 11:34:19 +0100

    modified with connection to db

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
+import os
+
 from flask import Flask
 from sqlalchemy import create_engine
 
 app = Flask(__name__)
 
+engine = create_engine(os.getenv('DATABASE_URL'))
+
 @app.route("/")
 def hello_world():
-    return "<p> Hello, World!</p>"
+    return f"DB string - {os.getenv('DATABASE_URL')}"
